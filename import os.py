d0ec033264bffab61b304d4d27cbdeb5060d007d
IshaqPro <101655476+IshaqPro@users.noreply.github.com>
2023-04-01 18:04:32 +0500

    Add files via upload

---
+++ import os.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+# .exe , .msi,  .gif, .png .jpg, .jpeg, .csv, .pdf , .xls , .xlsx , .ppt , .pptx
+from_dir = "C:/Users/Ishaq Pasha/Downloads"
+to_dir = "C:/WhiteHatJr/"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+
+    name, extension = os.path.splitext(file_name)
+    #print(name)
+    # #print(extension)
+
+    if extension == '':
+        continue
+    if extension in ['.txt','.pdf','.doc','.docx']:
+
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir + '/' + "Document_Files"
+        path3 = to_dir + '/' + "Document_files" + '/' + file_name
+        
+        if os.path.exists(path2):
+            print("Moving " + file_name + ".....")
+
+            shutl.move(path1, path3)
+
+        else:
+        os.makedirs(path2)
+        print("Moving " + file_name + ".....")
+        shutil.move(path1, path3)
